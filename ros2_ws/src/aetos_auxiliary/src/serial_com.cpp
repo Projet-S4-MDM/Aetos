bf646769e115b41e0b6aa2d78c9e7653a40cc1d9
emiles <emilesavoie39@gmail.com>
2025-01-29 13:41:29 -0500

    base for dynamic usb recognition

--- ros2_ws/src/aetos_auxiliary/src/serial_com.cpp
+++ ros2_ws/src/aetos_auxiliary/src/serial_com.cpp
@@ -12,19 +12,9 @@ public:
     SerialCom();
 
 private:
-    void topicCallback(const aetos_msgs::msg::Velocity::SharedPtr msg)
-    {
-        try
-        {
-            boost::asio::write(_serial, boost::asio::buffer(&msg->data[msg->VX], sizeof(float)));
-            boost::asio::write(_serial, boost::asio::buffer(&msg->data[msg->VY], sizeof(float)));
-            boost::asio::write(_serial, boost::asio::buffer(&msg->data[msg->VZ], sizeof(float)));
-        }
-        catch (const std::exception &e)
-        {
-            RCLCPP_ERROR(this->get_logger(), "Failed to send data: %s", e.what());
-        }
-    }
+    void topicCallback(const aetos_msgs::msg::Velocity::SharedPtr msg);
+    std::string usbMonitorCallback();
+    // void usbMonitorCallback(const std::string &port);
 
     boost::asio::io_service _io_service;
     boost::asio::serial_port _serial;
@@ -37,16 +27,13 @@ private:
     rclcpp::TimerBase::ConstSharedPtr _usbMonitorTimer;
 };
 
-SerialCom::SerialCom() : Node("serial_comm"), _serial(_io_service)
+void SerialCom::topicCallback(const aetos_msgs::msg::Velocity::SharedPtr msg)
 {
     try
     {
-        _serial.open("/dev/ttyUSB0");
-        _serial.set_option(boost::asio::serial_port_base::baud_rate(460800));
-        _serial.set_option(boost::asio::serial_port_base::character_size(8));
-        _serial.set_option(boost::asio::serial_port_base::parity(boost::asio::serial_port_base::parity::none));
-        _serial.set_option(boost::asio::serial_port_base::stop_bits(boost::asio::serial_port_base::stop_bits::one));
-        _serial.set_option(boost::asio::serial_port_base::flow_control(boost::asio::serial_port_base::flow_control::none));
+        boost::asio::write(_serial, boost::asio::buffer(&msg->data[msg->VX], sizeof(float)));
+        boost::asio::write(_serial, boost::asio::buffer(&msg->data[msg->VY], sizeof(float)));
+        boost::asio::write(_serial, boost::asio::buffer(&msg->data[msg->VZ], sizeof(float)));
     }
     catch (const std::exception &e)
     {
