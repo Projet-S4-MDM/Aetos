db5739d8408d5252c1f028dc6fc56659e2b9239a
Mc_DoDo <lebe0701@usherbrooke.ca>
2025-02-05 13:53:02 -0500

    Created the subscriber/publisher

--- ros2_ws/src/aetos_kinematic/src/velocity_conversion.cpp
+++ ros2_ws/src/aetos_kinematic/src/velocity_conversion.cpp
@@ -17,37 +17,60 @@
 #include <Eigen/Dense>
 
 #include "rclcpp/rclcpp.hpp"
-#include "aetos_msgs/msg/velocity.hpp"  // Include the generated header
+#include "aetos_msgs/msg/velocity.hpp"
+#include "aetos_msgs/msg/encoder_values.hpp"  // Include the generated header for EncoderValues
+#include "aetos_msgs/msg/motor_velocity.hpp"  // Include the generated header for MotorVelocity
 
 using std::placeholders::_1;
 
 class VelocityConversion : public rclcpp::Node
 {
 public:
-  VelocityConversionSubscriber()
+  VelocityConversion()
   : Node("VelocityConversion")
   {
-    subscription_ = this->create_subscription<aetos_msgs::msg::Velocity>(
-      "aetos/joy/velocity", 10, std::bind(&VelocityConversionSubscriber::topic_callback, this, _1));
+    sub_velocity = this->create_subscription<aetos_msgs::msg::Velocity>(
+      "aetos/joy/velocity", 10, std::bind(&VelocityConversion::velocity_callback, this, _1));
+
+    sub_encoder = this->create_subscription<aetos_msgs::msg::EncoderValues>(
+      "aetos/control/encoder", 10, std::bind(&VelocityConversion::encoder_callback, this, _1));
+
+    pub_motor_velocity = this->create_publisher<aetos_msgs::msg::MotorVelocity>("aetos/control/velocity", 10);  // Use the generated message type
   }
 
 private:
-  void topic_callback(const aetos_msgs::msg::Velocity & msg) const
+  float vx, vy, vz; // Vitesse
+  float x, y, z; // Position
+
+  void update_velocity(const aetos_msgs::msg::Velocity & msg);
+
+  void update_position();
+
+  void velocity_callback(const aetos_msgs::msg::Velocity & msg) const
   {
     RCLCPP_INFO(this->get_logger(), "I heard: velocity x: '%f', y: '%f', z: '%f'", msg.vx, msg.vy, msg.vz);
   }
-  rclcpp::Subscription<aetos_msgs::msg::Velocity>::SharedPtr subscription_;
+
+  void encoder_callback(const aetos_msgs::msg::EncoderValues & msg) const
+  {
+    RCLCPP_INFO(this->get_logger(), "I heard: encoder values");
+  }
+
+  rclcpp::Subscription<aetos_msgs::msg::Velocity>::SharedPtr sub_velocity;
+  rclcpp::Subscription<aetos_msgs::msg::EncoderValues>::SharedPtr sub_encoder;
+  rclcpp::Publisher<aetos_msgs::msg::MotorVelocity>::SharedPtr pub_motor_velocity;
 };
 
+void VelocityConversion::update_velocity(const aetos_msgs::msg::Velocity & msg){
+  vx = msg.vx;
+  vy = msg.vy;
+  vz = msg.vz;
+}
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<VelocityConversion>());
-  
-  
   rclcpp::shutdown();
-
-
   return 0;
 }
