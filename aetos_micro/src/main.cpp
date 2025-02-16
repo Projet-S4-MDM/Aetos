78f93c61f0cbd550ec2d6417d5575aec68dd6c6f
emiles <emilesavoie39@gmail.com>
2025-02-16 17:35:33 -0500

    Better structure for ros2 serial interface

--- aetos_micro/src/main.cpp
+++ aetos_micro/src/main.cpp
@@ -8,21 +8,21 @@
 
 void setup()
 {
-    Serial.begin(9600);
+    Serial.begin(115200);
 
-    FIT0186 motor1 = FIT0186(PIN_PWM_1, PIN_DIR_1, false);
-    Encoder encoder1 = Encoder(PIN_ENCODER_A1, PIN_ENCODER_B1);
-    PID pid1 = PID(1.0f, 0.0f, 0.0f, 0.0f);
+    // FIT0186 motor1 = FIT0186(PIN_PWM_1, PIN_DIR_1, false);
+    // Encoder encoder1 = Encoder(PIN_ENCODER_A1, PIN_ENCODER_B1);
+    // PID pid1 = PID(1.0f, 0.0f, 0.0f, 0.0f);
 
-    motor1.init();
+    // motor1.init();
 
-    for (;;)
-    {
-        motor1.setCmd(-10.0f);
-    }
-
+    // for (;;)
+    // {
+    //     motor1.setCmd(-10.0f);
+    // }
 }
 
 void loop()
 {
+
 }
--- ros2_ws/src/aetos_auxiliary/src/serial_com.cpp
+++ ros2_ws/src/aetos_auxiliary/src/serial_com.cpp
@@ -1,105 +1,190 @@
 #include <rclcpp/rclcpp.hpp>
 #include <aetos_msgs/msg/velocity.hpp>
-
 #include <libudev.h>
 #include <boost/asio.hpp>
 #include <iostream>
 #include <string>
+#include <mutex>
 
 class SerialCom : public rclcpp::Node
 {
 public:
-    SerialCom();
-
-    
-
-private:
-    void topicCallback(const aetos_msgs::msg::Velocity::SharedPtr msg);
-    std::string usbMonitorCallback();
-    // void usbMonitorCallback(const std::string &port);
-
-    boost::asio::io_service _io_service;
-    boost::asio::serial_port _serial;
-
-    std::string _serialPort;
-    std::thread _usbThread;
-    bool _serialConnected;
-
-    rclcpp::Subscription<aetos_msgs::msg::Velocity>::SharedPtr _motorVelSub;
-    rclcpp::TimerBase::ConstSharedPtr _usbMonitorTimer;
-};
-
-void SerialCom::topicCallback(const aetos_msgs::msg::Velocity::SharedPtr msg)
-{
-    try
+    SerialCom() : Node("serial_comm"), _serial(_io_service), _serialConnected(false)
     {
-        boost::asio::write(_serial, boost::asio::buffer(&msg->data[msg->VX], sizeof(float)));
-        boost::asio::write(_serial, boost::asio::buffer(&msg->data[msg->VY], sizeof(float)));
-        boost::asio::write(_serial, boost::asio::buffer(&msg->data[msg->VZ], sizeof(float)));
-    }
-    catch (const std::exception &e)
-    {
-        RCLCPP_ERROR(this->get_logger(), "Failed to send data: %s", e.what());
-    }
-}
+        _motorVelSub = this->create_subscription<aetos_msgs::msg::Velocity>(
+            "aetos/control/motor_speed", 10,
+            std::bind(&SerialCom::topicCallback, this, std::placeholders::_1));
 
-std::string SerialCom::usbMonitorCallback()
-{
-    struct udev *udev = udev_new();
+        _usbMonitorTimer = this->create_wall_timer(
+            std::chrono::milliseconds(500),
+            std::bind(&SerialCom::usbMonitorCallback, this));
 
-    if (!udev)
-    {
-        RCLCPP_ERROR(this->get_logger(), "Failed to initialize udev.");
-        return "";
+        _ioThread = std::thread([this]()
+                                {
+            while (rclcpp::ok()) {
+                try {
+                    _io_service.poll();
+                    _io_service.reset();
+                } catch (const std::exception& e) {
+                    RCLCPP_ERROR(this->get_logger(), "IO Service error: %s", e.what());
+                }
+            } });
     }
 
-    struct udev_enumerate *enumerate = udev_enumerate_new(udev);
-    udev_enumerate_add_match_subsystem(enumerate, "tty");
-    udev_enumerate_scan_devices(enumerate);
-
-    struct udev_list_entry *devices, *dev_list_entry;
-    devices = udev_enumerate_get_list_entry(enumerate);
-
-    std::string detected_port = "";
-
-    udev_list_entry_foreach(dev_list_entry, devices)
+    ~SerialCom()
     {
-        const char *path = udev_list_entry_get_name(dev_list_entry);
-        struct udev_device *dev = udev_device_new_from_syspath(udev, path);
-        if (dev)
+        closeSerialPort();
+        _io_service.stop();
+        if (_ioThread.joinable())
         {
-            const char *devnode = udev_device_get_devnode(dev);
-            if (devnode && (std::string(devnode).find("/dev/ttyUSB") != std::string::npos ||
-                            std::string(devnode).find("/dev/ttyACM") != std::string::npos))
-            {
-                detected_port = devnode;
-                udev_device_unref(dev);
-                break;
-            }
-            udev_device_unref(dev);
+            _ioThread.join();
         }
     }
 
-    udev_enumerate_unref(enumerate);
-    udev_unref(udev);
+private:
+    void topicCallback(const aetos_msgs::msg::Velocity::SharedPtr msg)
+    {
+        std::lock_guard<std::mutex> lock(_serialMutex);
+        if (!_serialConnected)
+        {
+            RCLCPP_WARN(this->get_logger(), "Serial port not connected. Dropping message.");
+            return;
+        }
 
-    return detected_port;
-}
+        try
+        {
+            // Format the data as a string with newline
+            std::string data = "VX:" + std::to_string(msg->data[msg->VX]) +
+                               " VY:" + std::to_string(msg->data[msg->VY]) +
+                               " VZ:" + std::to_string(msg->data[msg->VZ]) + "\n";
 
-SerialCom::SerialCom() : Node("serial_comm"), _serial(_io_service)
-{
-    _motorVelSub = this->create_subscription<aetos_msgs::msg::Velocity>(
-        "aetos/control/motor_speed", 10, std::bind(&SerialCom::topicCallback, this, std::placeholders::_1));
+            boost::asio::write(_serial, boost::asio::buffer(data));
+        }
+        catch (const std::exception &e)
+        {
+            RCLCPP_ERROR(this->get_logger(), "Failed to send data: %s", e.what());
+            _serialConnected = false; // Mark as disconnected to trigger reconnection
+        }
+    }
 
-    _usbMonitorTimer = this->create_wall_timer(
-        std::chrono::milliseconds(500), std::bind(&SerialCom::usbMonitorCallback, this));
-}
+    void usbMonitorCallback()
+    {
+        std::string detected_port = findSerialPort();
+
+        if (detected_port.empty())
+        {
+            if (_serialConnected)
+            {
+                RCLCPP_WARN(this->get_logger(), "Serial port disconnected");
+                closeSerialPort();
+            }
+            return;
+        }
+
+        if (!_serialConnected || _serialPort != detected_port)
+        {
+            std::lock_guard<std::mutex> lock(_serialMutex);
+            closeSerialPort();
+            if (openSerialPort(detected_port))
+            {
+                _serialPort = detected_port;
+                _serialConnected = true;
+                RCLCPP_INFO(this->get_logger(), "Connected to %s", detected_port.c_str());
+            }
+        }
+    }
+
+    bool openSerialPort(const std::string &port)
+    {
+        try
+        {
+            _serial.open(port);
+            _serial.set_option(boost::asio::serial_port_base::baud_rate(9600));
+            _serial.set_option(boost::asio::serial_port_base::character_size(8));
+            _serial.set_option(boost::asio::serial_port_base::stop_bits(boost::asio::serial_port_base::stop_bits::one));
+            _serial.set_option(boost::asio::serial_port_base::parity(boost::asio::serial_port_base::parity::none));
+            _serial.set_option(boost::asio::serial_port_base::flow_control(boost::asio::serial_port_base::flow_control::none));
+            return true;
+        }
+        catch (const std::exception &e)
+        {
+            RCLCPP_ERROR(this->get_logger(), "Failed to open serial port: %s", e.what());
+            return false;
+        }
+    }
+
+    void closeSerialPort()
+    {
+        if (_serial.is_open())
+        {
+            try
+            {
+                _serial.close();
+            }
+            catch (const std::exception &e)
+            {
+                RCLCPP_ERROR(this->get_logger(), "Error closing serial port: %s", e.what());
+            }
+        }
+        _serialConnected = false;
+    }
+
+    std::string findSerialPort()
+    {
+        struct udev *udev = udev_new();
+        if (!udev)
+        {
+            RCLCPP_ERROR(this->get_logger(), "Failed to initialize udev.");
+            return "";
+        }
+
+        std::string detected_port;
+        struct udev_enumerate *enumerate = udev_enumerate_new(udev);
+        udev_enumerate_add_match_subsystem(enumerate, "tty");
+        udev_enumerate_scan_devices(enumerate);
+
+        struct udev_list_entry *devices = udev_enumerate_get_list_entry(enumerate);
+        struct udev_list_entry *dev_list_entry;
+
+        for (dev_list_entry = devices; dev_list_entry != nullptr;
+             dev_list_entry = udev_list_entry_get_next(dev_list_entry))
+        {
+            const char *path = udev_list_entry_get_name(dev_list_entry);
+            struct udev_device *dev = udev_device_new_from_syspath(udev, path);
+            if (dev)
+            {
+                const char *devnode = udev_device_get_devnode(dev);
+                if (devnode && (std::string(devnode).find("/dev/ttyUSB") != std::string::npos ||
+                                std::string(devnode).find("/dev/ttyACM") != std::string::npos))
+                {
+                    detected_port = devnode;
+                    udev_device_unref(dev);
+                    break;
+                }
+                udev_device_unref(dev);
+            }
+        }
+
+        udev_enumerate_unref(enumerate);
+        udev_unref(udev);
+        return detected_port;
+    }
+
+    boost::asio::io_service _io_service;
+    boost::asio::serial_port _serial;
+    std::string _serialPort;
+    bool _serialConnected;
+    std::mutex _serialMutex;
+    std::thread _ioThread;
+
+    rclcpp::Subscription<aetos_msgs::msg::Velocity>::SharedPtr _motorVelSub;
+    rclcpp::TimerBase::SharedPtr _usbMonitorTimer;
+};
 
 int main(int argc, char *argv[])
 {
     rclcpp::init(argc, argv);
     rclcpp::spin(std::make_shared<SerialCom>());
     rclcpp::shutdown();
-
     return 0;
 }
