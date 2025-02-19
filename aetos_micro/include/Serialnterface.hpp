fb83b58b5717d0801a83de47645f8815b47f3755
emiles <emilesavoie39@gmail.com>
2025-02-19 09:44:38 -0500

    Serial interface class working

--- aetos_micro/include/Serialnterface.hpp
+++ aetos_micro/include/Serialnterface.hpp
@@ -26,7 +26,6 @@ public:
 
 private:
     sRequestedVelocity _requestedVelocity;
-     sRequestedVelocity defaultVelocity = {0.0f, 0.0f, 0.0f};
 
     Joint *_joint1 = nullptr;
     Joint *_joint2 = nullptr;
@@ -39,24 +38,29 @@ sRequestedVelocity SerialCom::getVelocityData()
     if (Serial.available() >= sizeof(sRequestedVelocity))
     {
         size_t bytesRead = Serial.readBytes(reinterpret_cast<char *>(&_requestedVelocity), sizeof(sRequestedVelocity));
-
-        return _requestedVelocity;
     }
-    return defaultVelocity;
+    return _requestedVelocity;
 }
 
 void SerialCom::sendEncoderData(Joint joint1_, Joint joint2_, Joint joint3_, Joint joint4_)
-{    
-    Serial.print(joint1_.getAngle());
+{
+    // Serial.print(joint1_.getAngle());
+    // Serial.print(",");
+    // Serial.print(joint2_.getAngle());
+    // Serial.print(",");
+    // Serial.print(joint3_.getAngle());
+    // Serial.print(",");
+    // Serial.println(joint4_.getAngle());
+    Serial.print("1.23");
     Serial.print(",");
-    Serial.print(joint2_.getAngle());
+    Serial.print("-0.56");
     Serial.print(",");
-    Serial.print(joint3_.getAngle());
+    Serial.print("2.78");
     Serial.print(",");
-    Serial.println(joint4_.getAngle());
+    Serial.println("18.05");
 }
 
-SerialCom::SerialCom(Joint *joint1_,Joint *joint2_,Joint *joint3_,Joint *joint4_)
+SerialCom::SerialCom(Joint *joint1_, Joint *joint2_, Joint *joint3_, Joint *joint4_)
     : _joint1(joint1_), _joint2(joint2_), _joint3(joint3_), _joint4(joint4_) {}
 
 #endif
--- aetos_micro/src/main.cpp
+++ aetos_micro/src/main.cpp
@@ -28,51 +28,16 @@ void setup()
         joint1.updateInternal();
         serialCom.sendEncoderData(joint1, joint2, joint3, joint4);
         // requestedVelocity = serialCom.getVelocityData();
-        // serialCom.sendEncoderData(joint1, joint2, joint3, joint4);
 
-        // joint1.
         // motor1.setCmd(30.0f);
         // requestedVelocity = serialCom.getVelocityData();
 
         // Serial.println(requestedVelocity.xVelocity);
         // Serial.println(requestedVelocity.yVelocity);
         // Serial.println(requestedVelocity.zVelocity);
-
-        // joint1.
     }
 }
 
 void loop()
 {
 }
-
-// void setup()
-// {
-//     Serial.begin(9600);
-
-//     for (;;)
-//     {
-//         if (Serial.available() >= sizeof(sRequestedVelocity))
-//         {
-//             sRequestedVelocity requestedVelocity;
-
-//             byte *dataPtr = reinterpret_cast<byte *>(&requestedVelocity);
-//             for (size_t i = 0; i < sizeof(sRequestedVelocity); i++)
-//             {
-//                 dataPtr[i] = Serial.read();
-//             }
-
-//             Serial.println("Received Velocity Data:");
-//             Serial.print("VX: ");
-//             Serial.println(requestedVelocity.xVelocity);
-//             Serial.print("VY: ");
-//             Serial.println(requestedVelocity.yVelocity);
-//             Serial.print("VZ: ");
-//             Serial.println(requestedVelocity.zVelocity);
-//         }
-//     }
-// }
-
-// void loop()
-// {
-// }
