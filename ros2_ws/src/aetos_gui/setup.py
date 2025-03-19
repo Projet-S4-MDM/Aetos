f13dcc0d66f8a417d360cc8173cfd5b70a458057
nikopapi <papn1801@usherbrooke.ca>
2025-03-19 13:12:27 -0400

    added .ui file path in setup.py, able to run aetos_gui w/o error

--- ros2_ws/src/aetos_gui/setup.py
+++ ros2_ws/src/aetos_gui/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'aetos_gui'
 
@@ -10,6 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/resource', glob('resource/*.ui')),  
     ],
     install_requires=['setuptools'],
     zip_safe=True,
