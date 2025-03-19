d20601ca150945466b799844ed8f741edea94382
nikopapi <papn1801@usherbrooke.ca>
2025-03-19 13:12:27 -0400

    added .ui file path in setup.py, able to run aetos_gui w/o error

--- ros2_ws/src/aetos_gui/aetos_gui/ui_node.py
+++ ros2_ws/src/aetos_gui/aetos_gui/ui_node.py
@@ -1,12 +1,17 @@
 import os
-from rclpy.node import Node
 from ament_index_python.packages import get_package_share_directory
+from rclpy.node import Node
 
 class UINode(Node):
     def __init__(self):
         super().__init__('ui_node')
 
     def get_resources_directory(self, package_name):
+        """Retourne le chemin du dossier 'resource' où se trouvent les fichiers UI."""
         package_share_directory = get_package_share_directory(package_name)
-        resource_directory = package_share_directory +"/../../resource/"
-        return resource_directory
+        resource_directory = os.path.join(package_share_directory, "resource")
+
+        if not os.path.exists(resource_directory):
+            self.get_logger().warn(f"Le répertoire des ressources n'existe pas : {resource_directory}")
+
+        return resource_directory + "/"
