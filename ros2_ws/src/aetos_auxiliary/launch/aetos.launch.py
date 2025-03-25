500c32fcd66cb0913061884707e2d832df918c91
nikopapi <papn1801@usherbrooke.ca>
2025-03-24 23:02:18 -0400

    added gui launch

--- ros2_ws/src/aetos_auxiliary/launch/aetos.launch.py
+++ ros2_ws/src/aetos_auxiliary/launch/aetos.launch.py
@@ -19,7 +19,12 @@ def generate_launch_description():
             PythonLaunchDescriptionSource([PathJoinSubstitution([FindPackageShare('aetos_simulation'), 'launch', 'sim.launch.py'])])),
 
         IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([PathJoinSubstitution([FindPackageShare('aetos_auxiliary'), 'launch', 'auxiliary.launch.py'])]))
+            PythonLaunchDescriptionSource([PathJoinSubstitution([FindPackageShare('aetos_auxiliary'), 'launch', 'auxiliary.launch.py'])])),
+        
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource([PathJoinSubstitution([FindPackageShare('aetos_gui'), 'launch', 'gui.launch.py'])]))
+        
+        
 
 
     ])
